f51fd7c9d2ad506ed1265444ac7193fdca6846ac
nicolas-brement <114388303+nicolas-brement@users.noreply.github.com>
2023-02-10 05:14:29 +0100

    Mise à jour

---
+++ Portfolio/animation.js
@@ -0,0 +1,82 @@
+  const hamburgerMenu = document.querySelector("#hamburger-menu");
+  const overlay = document.querySelector("#overlay");
+  const nav1 = document.querySelector("#nav-1");
+  const nav2 = document.querySelector("#nav-2");
+  const nav3 = document.querySelector("#nav-3");
+  const nav4 = document.querySelector("#nav-4");
+  const nav5 = document.querySelector("#nav-5");
+  const navItems = [nav1, nav2, nav3, nav4, nav5];
+  
+  // Control Navigation Animation
+  function navAnimation(val1, val2) {
+    navItems.forEach((nav, i) => {
+      nav.classList.replace(`slide-${val1}-${i + 1}`, `slide-${val2}-${i + 1}`);
+    });
+  }
+  
+  function toggleNav() {
+    // Toggle: Hamburger Open/Close
+    hamburgerMenu.classList.toggle("active");
+  
+    //   Toggle: Menu Active
+    overlay.classList.toggle("overlay-active");
+  
+    if (overlay.classList.contains("overlay-active")) {
+      // Animate In - Overlay
+      overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+  
+      // Animate In - Nav Items
+      navAnimation("out", "in");
+    } else {
+      // Animate Out - Overlay
+      overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
+  
+      // Animate Out - Nav Items
+      navAnimation("in", "out");
+    }
+  }
+  
+  // Events Listeners
+  hamburgerMenu.addEventListener("click", toggleNav);
+  navItems.forEach((nav) => {
+    nav.addEventListener("click", toggleNav);
+  });
+
+// Path: reveal.js
+  const ratio = .1
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: .1,
+    threshold: ratio
+  }
+
+  const handleIntersect = function (entries, observer) {
+    entries .forEach(function(entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible')
+            entry.target.classList.add('reveal2-visible')
+            console.log('visible')
+          } else {
+            entry.target.classList.remove('reveal-visible')
+            entry.target.classList.remove('reveal2-visible')
+            console.log('invisible')
+          }
+    });
+  }
+  
+  const observer = new IntersectionObserver(handleIntersect, options);
+  observer.observe(document.querySelector('.reveal'));
+
+  const observer2 = new IntersectionObserver(handleIntersect, options);
+  observer.observe(document.querySelector('.reveal2'));
+
+// LOADER
+
+/*
+window.addEventListener("load", function() {
+  setTimeout(function() {
+    document.getElementById("loader").classList.add("hidden");
+  }, 2500);
+});*/
+
---
+++ Portfolio/index.php
@@ -0,0 +1,245 @@
+<head>
+  <meta charset="utf-8">
+  <title>Portfolio - Nicolas Brement</title>
+  <link rel="stylesheet" href="style2.css">
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <link rel="shortcut icon" href="images/planet.png">
+</head>
+
+<body>
+
+  <header>
+<!-- MENU OVERLAY -->
+<div class="overlay overlay-slide-left" id="overlay">
+      <!-- MENU ITEMS -->
+      <nav>
+        <ul>
+          <li id="nav-1" class="slide-out-1 center">
+            <a href="#" class="center">Description</a>
+          </li>
+          <li id="nav-2" class="slide-out-2 center">
+            <a href="#section-tooling" class="center">Langages</a>
+          </li>
+          <li id="nav-3" class="slide-out-3 center">
+            <a href="#section-project" class="center">Projets</a>
+          </li>
+          <li id="nav-4" class="slide-out-4 center">
+            <a href="#section-networks" class="center">Networks</a>
+          </li>
+          <li id="nav-5" class="slide-out-5 center">
+            <a href="#section-contact" class="center">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <!-- HAMBURGER MENU -->
+    <div class="hamburger-menu" id="hamburger-menu">
+      <div class="menu-bar1"></div>
+      <div class="menu-bar2"></div>
+      <div class="menu-bar3"></div>
+    </div>
+</header>
+
+<section id="section-description">
+     <div class="container">
+            <div class="box1">
+                <div class="description reveal">
+                <h1 class="text-bg"><span>✌🏻</span>&nbsp;Hi there! I'm Nicolas Brement</h1>
+                    <h2>Web Developer Junior</h2><br>
+                    <p>Après avoir travaillé dans différents domaines artistiques et la vente,<br> j'ai décidé de me lancer
+                        dans l'univers du développement web.<br> J'ai intégré l'école de "La Plateforme_" et souhaite trouver une entreprise afin de pouvoir
+                        allier théorie et pratique.<br> Mon portfolio est le reflet de mon parcours et de mon savoir-faire en tant que développeur web.
+            </div>
+            </div>
+                   
+            <div class="avatar reveal2"><img src="images/avatar2nico.png"></div>
+                    
+        </div>
+    
+</section>
+
+    <section id="section-tooling">
+        <div class="tooling">
+            <div class="tooling-title" data-aos="flip-right" data-aos-duration="1500">
+                <h3>LANGAGES</h3><br>
+            </div>
+            <div class="tooling-box" data-aos="flip-up" data-aos-duration="1500">
+               <div class="box-content">
+                    <img src="images/htmlicon2.png" class="htmlicon"><h4>Html</h4>
+                </div>
+                <div class="box-content3">
+                    <img src="images/jsicon2.png" class="jsicon"><h4>Javascript</h4>
+                </div>
+                <div class="box-content2">
+                    <img src="images/cssicon2.png" class="cssicon"><h4>Css</h4>
+                </div>
+                <div class="box-content6">
+                    <img src="images/iconpython.png" class="pythonicon"><h4>Python</h4>
+                </div>
+                <div class="box-content4">
+                    <img src="images/phpicon.png" class="phpicon"><h4>Php</h4>
+                </div>
+                <div class="box-content5">
+                    <img src="images/sqlicon.png" class="sqlicon"><h4>Sql</h4>
+                </div>
+        </div>
+</section>
+
+<section id="section-project">
+    <div class="project-title-parent">
+            <div class="project-title" data-aos="flip-left" data-aos-duration="1500">
+                <h3>PROJETS</h3><br>
+            </div>
+    </div>
+
+<div class="project">
+
+
+<div class="project1" data-aos="zoom-in" data-aos-duration="1500">
+  <div class="card">
+    <div class="img"></div>
+
+    <rect fill="white" height="682.667" width="682.667"></rect>
+    <div class="text">
+      <p class="h3"> Fansite </p>
+      <p class="p"> Il s'agissait de créer un fansite sur le thème de notre choix, j'ai choisi de le faire
+        le Japon. </p>
+      <div class="button-wrap">
+            <button class="button"><a href="https://nicolas-brement.students-laplateforme.io/fansite/index.html">Visit →</a></button></div>
+    </div>
+  </div>
+  <!-- Fin du projet 1 -->
+</div>
+
+<div class="project2" data-aos="zoom-out" data-aos-duration="1500">
+  <div class="card">
+    <div class="img2"></div>
+
+    <rect fill="white" height="682.667" width="682.667"></rect>
+    <div class="text">
+      <p class="h3"> Réservation de salles </p>
+      <p class="p"> L'objectif était de créer un site de réservation de salles en PHP. La réservation n'est possible que si la personne est inscrite. </p>
+      <div class="button-wrap">
+            <button class="button"><a href="https://nicolas-brement.students-laplateforme.io/reservationsalles/index.php">Visit →</a></button></div>
+    </div>
+  </div>
+  <!-- Fin du projet 2 -->
+</div>
+
+<div class="project3" data-aos="zoom-in" data-aos-duration="1500">
+  <div class="card">
+    <div class="img3"></div>
+
+    <rect fill="white" height="682.667" width="682.667"></rect>
+    <div class="text">
+      <p class="h3"> Livre d'or </p>
+      <p class="p"> Site permettant à l'utilisateur de s'incrire, se connecter et laisser un commentaire sur le livre d'or. Réalisé sur le thème du manga Demon Slayer :) </p>
+      <div class="button-wrap">
+            <button class="button"><a href="https://nicolas-brement.students-laplateforme.io/livre-or/index.php">Visit →</a></button></div>
+    </div>
+  </div>
+  <!-- Fin du projet 3 -->
+</div>
+
+<div class="project4" data-aos="zoom-out" data-aos-duration="1500">
+  <div class="card">
+    <div class="img4"></div>
+
+    <rect fill="white" height="682.667" width="682.667"></rect>
+    <div class="text">
+      <p class="h3"> Module de connexion </p>
+      <p class="p"> Site permettant à l'utilisateur de s'incrire, se connecter et accéder à sa page profil afin de pouvoir le modifier ou le supprimer. Site réalisé sur le thème Studio Ghibli :) </p>
+      <div class="button-wrap">
+            <button class="button"><a href="https://nicolas-brement.students-laplateforme.io/moduleconnexion/index.php">Visit →</a></button></div>
+    </div>
+  </div>
+  <!-- Fin du projet 4 -->
+</div>
+
+<div class="project5" data-aos="zoom-in" data-aos-duration="1500">
+  <div class="card">
+    <div class="img5"></div>
+
+    <rect fill="white" height="682.667" width="682.667"></rect>
+    <div class="text">
+      <p class="h3"> Ville de Marseille </p>
+      <p class="p"> Premier travail de groupe dans lequel il était demandé de créer un site sur la ville de Marseille. </p>
+      <div class="button-wrap">
+            <button class="button"><a href="https://axel-vair.students-laplateforme.io/ville-marseille/index.html">Visit →</a></button></div>
+    </div>
+  </div>
+  <!-- Fin du projet 5 -->
+</div>
+
+</div>
+</section>
+
+<section id="section-networks">
+    <div class="networks-title-parent">
+            <div class="networks-title" data-aos="flip-right" data-aos-duration="1500">
+                <h3>NETWORKS</h3><br>
+            </div>
+    </div>
+  
+<div class="networks-icon">
+    <div class="box-cv" data-aos="fade-left" data-aos-duration="1500">
+      <a href="https://drive.google.com/file/d/1mVXu5vZDqQXixITvEBw16eBwx9Bt9Tom/view?usp=sharing"><img id="CV" src="images/iconcv1.png" alt="photo CV"></a><h5>CV</h5>
+    </div>
+    <div class="box-linkedin" data-aos="fade-down" data-aos-duration="1500">
+      <a href="https://www.linkedin.com/in/nicolas-brement-3778a9260/"><img id="linkedin" src="images/iconlinkedin1.png" alt="photo linkedin"></a><h5>Linkedin</h5>
+    </div>
+    <div class="box-github" data-aos="fade-right" data-aos-duration="1500">
+      <a href="https://github.com/nicolas-brement"><img id="github" src="images/githubicon.png" alt="photo linkedin"></a><h5>Github</h5>
+    </div>
+</div>
+
+<div class="networks-name">
+  <div class="text-CV">
+    <a href="https://drive.google.com/file/d/1mVXu5vZDqQXixITvEBw16eBwx9Bt9Tom/view?usp=sharing"><h5>CV</h5></a>
+  </div>
+  <div class="text-linkedin">
+    <a href="https://www.linkedin.com/in/nicolas-brement-3778a9260/"><h5>Linkedin</h5></a>
+  </div>
+  <div class="text-github">
+    <a href="https://github.com/nicolas-brement"><h5>Github</h5></a>
+  </div>
+</div>
+</section>
+
+<section id="section-contact">
+    <div class="contact">
+            <div class="contact-title" data-aos="flip-right" data-aos-duration="1500">
+                <h3>CONTACT</h3><br>
+            </div>
+            <div class="question-contact" data-aos="zoom-in" data-aos-duration="1500">
+                <p>{&nbsp;</p><h6>Une&nbsp;question?</h6><p>&nbsp;}</p>
+            </div>
+            <div class="text-contact" data-aos="zoom-out" data-aos-duration="1500">
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Si vous avez des questions
+                <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ou que mon profil vous interesse,
+                <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;contactez-moi. 😇<br>
+                    &nbsp;Je vous répondrai dans les plus brefs délais.</p>
+
+                <button class="bouton-contact">
+                <a href="mailto:nicolas.brement@laplateforme.io">Me contacter</a>
+                </button>
+</section>
+
+<footer>
+    <div class="footer">
+        <div class="footer-text">
+            <p>© 2023 Nicolas Brement</p>
+        </div>
+    </div>
+</footer>
+
+    <script>
+        AOS.init();
+    </script>
+</body>
+
+
+<script src="animation.js"></script>
+
+
